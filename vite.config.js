8577044c2363f47598654f26ad9ee368e6d92bf7
Thunderk3g <diwakar29buddy@gmail.com>
2026-02-13 16:09:13 +0530

    feat: Configure Vite base URL, build output options, and development server port.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [react()],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            output: {
+                name: 'QuizGame',
+                exports: 'named',
+                format: 'es'
+            }
+        }
+    },
+    server: {
+        port: 5003 // Development port
+    }
 })
